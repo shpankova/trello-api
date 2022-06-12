ed5ffb9c5fcb0ce6469219088fe139d4bfae4838
spankova <dshpankova282002@gmail.com>
2022-06-12 21:10:16 +0300

    sql create db and tables

---
+++ sql/createTable.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS "TrelloSchema";
+
+CREATE TABLE IF NOT EXISTS "TrelloSchema"."board"
+(   "board_id" SERIAL PRIMARY KEY, 
+    "name" text NOT NULL,
+	"color" text NOT NULL,
+    "description" text NOT NULL,
+    "create_at" timestamp without time zone NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "TrelloSchema"."card"
+(   "card_id" SERIAL PRIMARY KEY,
+    "name" text NOT NULL,
+    "description" text NOT NULL,
+    "create_at" timestamp without time zone NOT NULL,
+    "estimate" text,
+    "status" text NOT NULL,
+    "due_date" timestamp without time zone NOT NULL,
+    labels text NOT NULL
+);
