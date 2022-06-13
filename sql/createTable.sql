36ee74cf40f27bc4a64d8f82ff0883cbeb847411
spankova <dshpankova282002@gmail.com>
2022-06-13 15:13:56 +0300

    update tables

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -10,21 +10,14 @@ CREATE TABLE IF NOT EXISTS "TrelloSchema"."board"
 
 CREATE TABLE IF NOT EXISTS "TrelloSchema"."card"
 (   "card_id" SERIAL PRIMARY KEY,
+    "board_id" integer NOT NULL,
     "name" text NOT NULL,
     "description" text NOT NULL,
     "create_at" timestamp without time zone NOT NULL,
     "estimate" text,
     "status" text NOT NULL,
     "due_date" timestamp without time zone NOT NULL,
-    labels text NOT NULL
+    labels text NOT NULL,
+    FOREIGN KEY (board_id) REFERENCES "TrelloSchema"."board" (board_id)
 );
 
-CREATE TABLE IF NOT EXISTS "TrelloSchema".boardcard
-(
-	id SERIAL NOT NULL,
-    board_id integer NOT NULL,
-	card_id integer NOT NULL,
-	FOREIGN KEY (board_id) REFERENCES "TrelloSchema".board (board_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE,
-	FOREIGN KEY (card_id) REFERENCES "TrelloSchema".card (card_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE
-    
-);
