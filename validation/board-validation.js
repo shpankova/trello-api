b2b8521b3884eb2dceea29dd070e36f03adb5a6a
spankova <dshpankova282002@gmail.com>
2022-06-15 17:02:00 +0300

    updated validation schema

--- validation/board-validation.js
+++ validation/board-validation.js
@@ -6,6 +6,8 @@ exports.boardValidation = (data) => {
         name : Joi.string().max(40).required(),
         color : Joi.string().max(40).required(),
         description : Joi.string().required(),
+        role: Joi.string().required(),
+        board_id: Joi.number().integer()
         })
     return schema.validate(data)
 }
--- validation/card-validation.js
+++ validation/card-validation.js
@@ -3,14 +3,14 @@ const Joi = require('joi');
 exports.cardValidation = (data) => {
 
     const schema = Joi.object({
-        board_id: Joi.number().integer().required(),
+        board_id: Joi.number().integer(),
         name : Joi.string().max(40).required(),
         description : Joi.string().required(),
         estimate: Joi.string().required(),
         status: Joi.string().max(40).required(),
         due_date: Joi.date().timestamp().required(),
         labels: Joi.string().max(40).required(),
-        card_id: Joi.number().integer().required()
+        card_id: Joi.number().integer()
         })
     return schema.validate(data)
 }
