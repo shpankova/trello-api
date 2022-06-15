5ad238415b9aa7ca2f95735245c9e7d54340d649
spankova <dshpankova282002@gmail.com>
2022-06-15 15:46:19 +0300

    update card`s validation

--- validation/card-validation.js
+++ validation/card-validation.js
@@ -9,7 +9,8 @@ exports.cardValidation = (data) => {
         estimate: Joi.string().required(),
         status: Joi.string().max(40).required(),
         due_date: Joi.date().timestamp().required(),
-        labels: Joi.string().max(40).required()
+        labels: Joi.string().max(40).required(),
+        card_id: Joi.number().integer().required()
         })
     return schema.validate(data)
 }
