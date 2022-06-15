cdc9e777c0a797a18a7d4f664270c882017f0530
spankova <dshpankova282002@gmail.com>
2022-06-15 13:11:01 +0300

    added error middleware

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
 
+
 const routerCard = require('./routes/card')
 const routerBoard = require('./routes/board')
+const errorMiddleware = require('./middlewares/error-middleware')
+
 
 const PORT = process.env.PORT || 7000;
 const HOST = process.env.HOST || '127.0.0.1';
@@ -11,11 +14,13 @@ const HOST = process.env.HOST || '127.0.0.1';
 const app = express();
 
 app.use(express.json({ extended: true }))
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use('/api/card', routerCard)
 app.use('/api/board', routerBoard)
 
 app.use(helmet())
+app.use(errorMiddleware)
+
 
 app.listen(PORT, HOST, () => {
     console.log(`Server listens http://${HOST}:${PORT}`);
