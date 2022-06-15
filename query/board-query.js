3cf3885e4fb5eabed53467e41c98ec4d120966ef
spankova <dshpankova282002@gmail.com>
2022-06-15 13:13:44 +0300

    updated queries

--- query/board-query.js
+++ query/board-query.js
@@ -1,36 +1,36 @@
 const createBoard = `
-INSERT INTO
-    "TrelloSchema"."board" 
-    ( name, color, description, create_at) 
-VALUES 
-    ($1, $2, $3, $4)`;
+    INSERT INTO
+        "TrelloSchema"."board" 
+        ( name, color, description ) 
+    VALUES 
+        ($1, $2, $3)`;
 
 const findBoardById = `
-SELECT *
-FROM 
-    "TrelloSchema"."board" 
-JOIN 
-    "TrelloSchema"."card" 
-ON 
-    "TrelloSchema"."board"."board_id"="TrelloSchema"."card"."board_id" 
-WHERE 
-    "TrelloSchema"."board"."board_id" = $1  `;
+    SELECT *
+    FROM 
+        "TrelloSchema"."board" 
+    JOIN 
+        "TrelloSchema"."card" 
+    ON 
+        "TrelloSchema"."board"."board_id"="TrelloSchema"."card"."board_id" 
+    WHERE 
+        "TrelloSchema"."board"."board_id" = $1  `;
+
 
 const updateBoardById = `
-UPDATE "TrelloSchema"."board" 
-SET 
-    name = $1, 
-    color = $2,
-    description = $3, 
-    create_at = $4, 
-WHERE 
-    board_id = $7`;
+    UPDATE "TrelloSchema"."board" 
+    SET 
+        name = $1, 
+        color = $2,
+        description = $3
+    WHERE 
+        board_id = $4`;
 
 const deleteBoardById = `
-DELETE FROM 
-    "TrelloSchema"."board" 
-WHERE 
-    board_id = $1`;
+    DELETE FROM 
+        "TrelloSchema"."board" 
+    WHERE 
+        board_id = $1`;
 
 
 module.exports = {createBoard, findBoardById, updateBoardById, deleteBoardById};
--- query/card-query.js
+++ query/card-query.js
@@ -1,45 +1,53 @@
 const createCard = `
-INSERT INTO 
-    "TrelloSchema"."card" 
-    ( board_id, name, description, create_at, status, due_date, labels) 
-VALUES 
-    ($1, $2, $3, $4, $5, $6, $7)`;
+    INSERT INTO 
+        "TrelloSchema"."card" 
+        ( board_id, name, description, estimate, status, due_date, labels) 
+    VALUES 
+        ($1, $2, $3, $4, $5, $6, $7)`;
 
 const findCardById = `
-SELECT 
-    card_id,
-    board_id,
-    name, 
-    description,
-    create_at,
-    status,
-    due_date,
-    labels
+    SELECT 
+        card_id,
+        board_id,
+        name, 
+        description,
+        create_at,
+        status,
+        due_date,
+        labels
+    FROM 
+        "TrelloSchema"."card" 
+    WHERE 
+        card_id = $1`;
+
+const findCard = `
+SELECT EXISTS ( SELECT 1
 FROM 
     "TrelloSchema"."card" 
 WHERE 
-    card_id = $1`;
+"TrelloSchema"."card"."name" = $1)`;
+    
 
 
 const updateCardById = `
-UPDATE 
-    "TrelloSchema"."card" 
-SET 
-    board_id =$1,
-    name = $2, 
-    description = $3, 
-    create_at = $4, 
-    status = $5, 
-    due_date = $6,
-    labels = $7
-WHERE 
-    card_id = $8`;
+    UPDATE 
+        "TrelloSchema"."card" 
+    SET 
+        board_id =$1,
+        name = $2, 
+        description = $3,
+        estimate = $4,  
+        status = $5, 
+        due_date = $6,
+        labels = $7
+    WHERE 
+        card_id = $8`;
 
 const deleteCardById = `
-DELETE FROM 
-    "TrelloSchema"."card" 
-WHERE 
-    card_id = $1`;
+    DELETE FROM 
+        "TrelloSchema"."card" 
+    WHERE 
+        card_id = $1`;
 
 
-module.exports = { createCard, findCardById, updateCardById, deleteCardById }
+module.exports = { createCard, findCardById, updateCardById, deleteCardById, findCard }
