05e995d64161503f81a73b57ecb35f8b98e760f4
spankova <dshpankova282002@gmail.com>
2022-06-20 09:55:13 +0300

    separated the app fron the server

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const helmet = require('helmet')
+
+
+const routerCard = require('./routes/card')
+const routerBoard = require('./routes/board')
+const errorMiddleware = require('./middlewares/error-middleware')
+
+
+const app = express();
+
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', routerCard)
+app.use('/api', routerBoard)
+
+app.use(helmet())
+app.use(errorMiddleware)
+
+module.exports = app
