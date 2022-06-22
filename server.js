15906c4676a1df55e06a30b6058304dcc38878c5
spankova <dshpankova282002@gmail.com>
2022-06-22 17:31:50 +0300

    updated server.js with using config.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require('./app')
+const config = require('./config.js');
 
-const PORT = process.env.PORT || 8000;
-const HOST = process.env.HOST || '0.0.0.0';
+const PORT = config.PORT || 8000;
+const HOST = config.HOST || '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
     console.log(`Server listens http://${HOST}:${PORT}`);
